4cae19fc44aebb2141ff4ccdba35ffadc518df55
aksoyarif <44071751+arifaksoy@users.noreply.github.com>
2021-12-02 01:22:30 +0300

    vueJs-2
    commit vue js exercise 2

---
+++ 2/main.js
@@ -0,0 +1,11 @@
+const app= Vue.createApp({
+data(){
+    return{
+        product:'socks',
+        image:'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+        url:'https://www.google.com/',
+        onSale:true
+    }
+}
+
+});
